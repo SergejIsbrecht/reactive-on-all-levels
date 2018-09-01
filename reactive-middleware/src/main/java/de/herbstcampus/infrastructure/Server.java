a3f359c49f626a937a05d16d46dbb817d63e3db6
seis7397 <sergej.isbrecht@esolutions.de>
2018-09-01 14:28:49 +0200

    Subscribe to own topic for testing

--- reactive-middleware/src/main/java/de/herbstcampus/infrastructure/Server.java
+++ reactive-middleware/src/main/java/de/herbstcampus/infrastructure/Server.java
@@ -3,16 +3,19 @@ package de.herbstcampus.infrastructure;
 import de.herbstcampus.topic.HighBeamAssistTopic;
 import de.herbstcampus.topic.IndicatorTopic;
 import de.herbstcampus.topic.SpeedTopic;
+import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
+import io.rsocket.transport.netty.client.WebsocketClientTransport;
 import io.rsocket.transport.netty.server.NettyContextCloseable;
 import io.rsocket.transport.netty.server.WebsocketServerTransport;
+import io.rsocket.util.DefaultPayload;
 import io.vavr.collection.Array;
 import javax.annotation.ParametersAreNonnullByDefault;
 import reactor.core.scheduler.Schedulers;
 
 @ParametersAreNonnullByDefault
 public final class Server {
-  static void main(String[] args) {
+  public static void main(String[] args) {
     RemoteSampleFacadeFactory remoteSampleFacadeFactory = new RemoteSampleFacadeFactory("192.168.1.1", Schedulers.single());
 
     SpeedSensor speedSensor = new SpeedSensor(remoteSampleFacadeFactory);
@@ -25,11 +28,15 @@ public final class Server {
     IndicatorTopic indicatorTopic = new IndicatorTopic(indicatorSensor);
     SpeedTopic speedTopic = new SpeedTopic(speedSensor);
 
-    NettyContextCloseable localhost =
+    NettyContextCloseable webSocketServer =
         RSocketFactory.receive() //
+            .errorConsumer(Throwable::printStackTrace)
             .acceptor(new SocketAcceptorImpl(Array.of(highBeamAssistTopic, indicatorTopic, speedTopic)))
             .transport(WebsocketServerTransport.create("localhost", 6666))
             .start()
             .block();
+
+    RSocket socket = RSocketFactory.connect().transport(WebsocketClientTransport.create("localhost", 6666)).start().block();
+    socket.requestStream(DefaultPayload.create(speedTopic.name())).doOnNext(payload -> System.out.println(payload.getDataUtf8())).blockLast();
   }
 }
