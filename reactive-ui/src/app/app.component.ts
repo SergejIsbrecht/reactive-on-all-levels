92e54d81fae04f992f1a1b1a61a9d4db64df2bb0
Unknown <Dominik.Engelmann@esolutions.de>
2018-09-12 14:53:15 +0200

    Layout: New design for config area
    * Switch instead of buttons
    * method changed for events of switch
    * string changed to boolean

--- reactive-ui/src/app/app.component.ts
+++ reactive-ui/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import {Component, Injector} from '@angular/core';
-import {interval, Observable} from 'rxjs';
-import {HighBeamState} from './highBeamState';
-import {IndicatorType} from './indicatorType';
-import {switchMap} from 'rxjs/operators';
-import {CarKeyboardService} from './car-keyboard.service';
-import {CarService} from './car.service';
-import {CarRestService} from './car-rest.service';
-import {map, pairwise, takeWhile} from 'rxjs/internal/operators';
+import { Component, Injector } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { HighBeamState } from './highBeamState';
+import { IndicatorType } from './indicatorType';
+import { switchMap } from 'rxjs/operators';
+import { CarKeyboardService } from './car-keyboard.service';
+import { CarService } from './car.service';
+import { CarRestService } from './car-rest.service';
+import { map, pairwise, takeWhile } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-root',
@@ -17,26 +17,33 @@ export class AppComponent {
   highBeam$: Observable<HighBeamState>;
   speed$: Observable<string>;
   indicator$: Observable<IndicatorType>;
-  serviceBackend = 'keyboard';
+  rest = false;
 
   constructor(private injector: Injector) {
     this.updateServiceBackend();
   }
 
+  toggleConfig() {
+    this.rest = !this.rest;
+    this.updateServiceBackend();
+  }
+
   updateServiceBackend() {
     let carService: CarService;
-    if (this.serviceBackend === 'keyboard') {
+    if (!this.rest) {
       carService = this.injector.get(CarKeyboardService);
-    } else if (this.serviceBackend === 'rest') {
+    } else if (this.rest) {
       carService = this.injector.get(CarRestService);
     }
 
     this.highBeam$ = carService.highBeam();
-    this.indicator$ = carService.indicator().pipe(
-      switchMap(s =>
-        interval(500).pipe(map(i => (i + 1) % 2 ? s : IndicatorType.OFF))
-      )
-    );
+    this.indicator$ = carService
+      .indicator()
+      .pipe(
+        switchMap(s =>
+          interval(500).pipe(map(i => ((i + 1) % 2 ? s : IndicatorType.OFF)))
+        )
+      );
     this.speed$ = carService.speed().pipe(
       pairwise(),
       switchMap(data => {
@@ -44,7 +51,7 @@ export class AppComponent {
         const targetSpeed: number = data[1];
         return interval(20).pipe(
           map(i => Math.floor((targetSpeed - prevSpeed) / 25) * i + prevSpeed),
-          takeWhile((currSpeed) => {
+          takeWhile(currSpeed => {
             if (prevSpeed <= targetSpeed) {
               return currSpeed <= targetSpeed;
             } else {
@@ -53,6 +60,7 @@ export class AppComponent {
           })
         );
       }),
-      map(String));
+      map(String)
+    );
   }
 }
